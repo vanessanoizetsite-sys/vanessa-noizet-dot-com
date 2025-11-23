4be934e50aa305d3cff1d2d387910ff1364e8aec
achtaitaipai <charles.cailleteau@galadrim.fr>
2025-11-23 12:13:12 +0100

    chore: add astro-typesafe-routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+import typesafeRoutes from "astro-typesafe-routes";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [typesafeRoutes()],
 });
