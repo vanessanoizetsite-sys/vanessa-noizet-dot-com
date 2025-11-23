e6b3c7248e4e6b46824506f5aea3b1ff6ebcc431
achtaitaipai <charles.cailleteau@galadrim.fr>
2025-11-23 12:18:10 +0100

    style: format code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "astro/config";
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'astro/config'
 
-import typesafeRoutes from "astro-typesafe-routes";
+import typesafeRoutes from 'astro-typesafe-routes'
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
+	vite: {
+		plugins: [tailwindcss()],
+	},
 
-  integrations: [typesafeRoutes()],
-});
+	integrations: [typesafeRoutes()],
+})
